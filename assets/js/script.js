7684436e7d7a8ba5a4b54755ef289c4a1b466914
Rakesh Kumar Sahu <131417933+Rakeshkumarsahugithub@users.noreply.github.com>
2025-02-20 12:59:05 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Rakesh Portfolio";
+            document.title = "👨‍💻Rakesh Portfolio";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
